69b2546687f5b9bf775f430985640f5584bfe5c8
Tybouddha <bruneecedric@gmail.com>
2024-10-02 17:25:19 +0200

    front with ad back

--- components/Home.js
+++ components/Home.js
@@ -33,17 +33,9 @@ function Home() {
     </div>
   );
 
-  // Movies list
-  // const moviesData = [
-  //   { title: 'Forrest Gump', poster: 'forrestgump.jpg', voteAverage: 9.2, voteCount: 22_705, overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.' },
-  //   { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
-  //   { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
-  //   { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
-  //   { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
-  // ];
   const [moviesData, setMoviesData]=useState([]);
   useEffect(() => {
-    fetch(`http://localhost:3000/movies`)
+    fetch(`https://mymoviz-backend-phi-ten.vercel.app/movies`)
     .then(response => response.json())
     .then(apiData => {
       setMoviesData(apiData.movies);
